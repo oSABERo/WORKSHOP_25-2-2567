dc99e7059735123da412dc96a8ac3687d565dfc5
oSABERo <zaqsf159359@gmail.com>
2024-02-25 11:06:33 +0700

    Add sex mapping to insurance.py

--- insurance.py
+++ insurance.py
@@ -6,3 +6,11 @@ df = pd.read_csv('insurance.csv')
 print(df.info())
 
 print(df['sex'].unique())
+
+#กำหนดการแปบว
+sex_map = {
+    'female': 0,
+    'male': 1
+}
+df['sex']=df['sex'].map(sex_map) 
+print(df.head())
