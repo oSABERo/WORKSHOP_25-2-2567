fc154116f483b448f6d11ba92b06fed1472c9ea2
oSABERo <zaqsf159359@gmail.com>
2024-02-25 12:38:48 +0700

    Refactor power.py: Import pandas, drop 'ts' column, select specific columns, and print DataFrame info

--- power.py
+++ power.py
@@ -1,8 +1,14 @@
-#EXAMPLE1
+# Import the pandas library as pd
 import pandas as pd
 
-#อ่านไฟล์
+# Read the CSV file named 'power.csv' and store the data in a DataFrame named 'df'
 df = pd.read_csv('power.csv')
+
+# Drop the 'ts' column from the DataFrame
 df = df.drop(['ts'], axis=1)
+
+# Select only the 'irr', 'tamb', 'tmodule', 'wind', and 'pack' columns from the DataFrame
 df = df[['irr', 'tamb', 'tmodule', 'wind', 'pack']]
+
+# Print information about the DataFrame, including the number of entries, the number of non-null entries for each column, the column data types, and memory usage
 print(df.info())
