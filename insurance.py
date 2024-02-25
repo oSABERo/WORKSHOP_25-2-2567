94be127f2ec68734296e15f9bd140f16e7ea069f
oSABERo <zaqsf159359@gmail.com>
2024-02-25 11:11:53 +0700

    #update

--- insurance.py
+++ insurance.py
@@ -7,10 +7,21 @@ print(df.info())
 
 print(df['sex'].unique())
 
-#กำหนดการแปบว
+#กำหนดการแปลง
 sex_map = {
     'female': 0,
     'male': 1
 }
 df['sex']=df['sex'].map(sex_map) 
+
+smoker_map = {
+    'yes': 1,
+    'no': 0
+}
+df['smoker']=df['smoker'].map(smoker_map)
+
+region_encoded = pd.get_dummies(df, columns['region'])
+df = pd.concat([df, region_encoded], axis=1)
+df = df.drop('region', axis=1)
+
 print(df.head())
